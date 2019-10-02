1eac3903dbfe0fed0d323d61161ed08e8274e2b5
jaeho <jaeho7224@naver.com>
2019-10-02 13:24:54 +0900

    11

---
+++ pro10/src/sec03/ex01/EncoderFilter.java
@@ -0,0 +1,46 @@
+package sec03.ex01;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Servlet Filter implementation class EncoderFilter
+ */
+@WebFilter("/*")
+public class EncoderFilter implements Filter {
+ServletContext context;
+	public void init(FilterConfig fConfig) throws ServletException{
+		System.out.println("utf-8 인코딩...........");
+		context =fConfig.getServletContext();
+	}
+
+		/**
+		 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+		 */
+		public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+			System.out.println("doFilter호출");
+			request.setCharacterEncoding("utf-8");
+			String context=((HttpServletRequest)request).getContextPath();
+			String pathinfo=((HttpServletRequest)request).getRequestURI();
+			String realPath = request getRealPath(pathinfo);
+			String mesg="Context 정보:"+context
+					+"\n URI정보:" +pathinfo
+					+"\n 물리적 경로"+realPath;
+			System.out.println(mesg);
+			chain.doFilter(request, response);
+		}
+
+		public void destroy() {
+			System.out.println("destroy호출");
+		}
+
+
+}
